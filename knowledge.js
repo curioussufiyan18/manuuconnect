da5bb677aa39195da01309c392b40baa5c541183
curioussufiyan18 <pvtsufiyan18@gmail.com>
2026-08-16 17:33:35 +0530

    Create knowledge.js

---
+++ knowledge.js
@@ -0,0 +1,17 @@
+import about from "./knowledge/about.json" with { type: "json" };
+import coreteam from "./knowledge/coreteam.json" with { type: "json" };
+import events from "./knowledge/events.json" with { type: "json" };
+import projects from "./knowledge/projects.json" with { type: "json" };
+import achievements from "./knowledge/achievements.json" with { type: "json" };
+import mentors from "./knowledge/mentors.json" with { type: "json" };
+import faq from "./knowledge/faq.json" with { type: "json" };
+
+export const knowledge = {
+  about,
+  coreteam,
+  events,
+  projects,
+  achievements,
+  mentors,
+  faq,
+};
