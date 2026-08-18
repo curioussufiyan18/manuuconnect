1f3c2f35b02757c90a103e25ea370b530ea91513
curioussufiyan18 <pvtsufiyan18@gmail.com>
2026-08-19 00:30:12 +0530

    Update knowledge.js

--- knowledge.js
+++ knowledge.js
@@ -1,17 +1,23 @@
 import about from "./knowledge/about.json";
-import coreteam from "./knowledge/coreteam.json";
+import team from "./knowledge/team.json";
 import events from "./knowledge/events.json";
+import mentors from "./knowledge/mentors.json";
+import faculty from "./knowledge/faculty.json";
 import projects from "./knowledge/projects.json";
 import achievements from "./knowledge/achievements.json";
-import mentors from "./knowledge/mentors.json";
+import internship from "./knowledge/internship.json";
+import referrals from "./knowledge/referrals.json";
 import faq from "./knowledge/faq.json";
 
 export const knowledge = {
   about,
-  coreteam,
+  team,
   events,
+  mentors,
+  faculty,
   projects,
   achievements,
-  mentors,
-  faq,
+  internship,
+  referrals,
+  faq
 };
